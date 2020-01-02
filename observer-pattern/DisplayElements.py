e9442b4f66be169dc6338005e633e755b786b5ef
loyx <30404367+loyx@users.noreply.github.com>
2020-01-02 15:23:02 +0800

    add StatisticDisplay

--- observer-pattern/DisplayElements.py
+++ observer-pattern/DisplayElements.py
@@ -18,3 +18,31 @@ class CurrentConditionDisplay(Observer, DisplayElement):
         print("Current conditions:{}F degrees and {}% humidity".
               format(self.temperature, self.humidity))
 
+
+class StatisticsDisplay(Observer, DisplayElement):
+
+    def __init__(self, weather_data):
+        self.observable = weather_data
+        self.observable.registerObserver(self)
+        self.max_temperature = -0x3f3f3f
+        self.min_temperature = 0x3f3f3f
+        self.avg_temperature = 0
+        self.__cont_record = 0
+
+    def update(self):
+        curr_temperature = self.observable.getTemperature()
+        if curr_temperature > self.max_temperature:
+            self.max_temperature = curr_temperature
+        if curr_temperature < self.min_temperature:
+            self.min_temperature = curr_temperature
+        self.avg_temperature = (self.avg_temperature * self.__cont_record +
+                                curr_temperature) / (self.__cont_record+1)
+        self.__cont_record += 1
+        self.display()
+
+    def display(self):
+        print("Avg/Max/Min temperature:{}/{}/{}".format(
+            self.avg_temperature,
+            self.max_temperature,
+            self.min_temperature
+        ))
--- observer-pattern/WeatherStation.py
+++ observer-pattern/WeatherStation.py
@@ -1,9 +1,10 @@
 from WeatherData import WeatherData
-from DisplayElements import CurrentConditionDisplay
+from DisplayElements import *
 
 weatherData = WeatherData()
 
 currentConditionDisplay = CurrentConditionDisplay(weatherData)
+statisticsDisplay = StatisticsDisplay(weatherData)
 
 weatherData.setMeasurements(80, 65, 30.4)
 weatherData.setMeasurements(82, 70, 29.2)
